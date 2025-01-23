41d9ca70d73a17aab9c7be6ed3480421cc7726ec
Aymen Hmid <48675135+aymenhmid@users.noreply.github.com>
2025-01-23 10:40:21 +0100

    Create contains_duplicate.py

---
+++ Arrays_and_Hashing/contains_duplicate.py
@@ -0,0 +1,32 @@
+#Brute Force
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+#Sorting
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
+
+#Hash Set
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+#Hash Set Length
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
